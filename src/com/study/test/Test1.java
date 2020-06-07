f3d8c12f6b81add86665f17b28ead1b7f5d1238c
李道奇 <lidaoqideyouxiang@163.com>
2020-06-07 14:03:11 +0800

    第一次测试代上传到远程git

---
+++ src/com/study/test/Test1.java
@@ -0,0 +1,11 @@
+package com.study.test;
+
+import java.util.Date;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.getTime());
+        System.out.println(date.getTime());
+    }
+}
