1c883ade3675ae49dfe6022f5388e57d922c1102
李道奇 <lidaoqideyouxiang@163.com>
2020-06-07 14:13:26 +0800

    第二次测试github

--- src/com/study/test/Test1.java
+++ src/com/study/test/Test1.java
@@ -6,6 +6,7 @@ public class Test1 {
     public static void main(String[] args) {
         Date date = new Date();
         System.out.println(date.getTime());
+        System.out.println("我是谁?");
         System.out.println(date.getTime());
     }
 }
